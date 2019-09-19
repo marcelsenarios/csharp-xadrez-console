7bba0267f3c88f02618fd2ea7f039bd81e2f1971
Carlos Marcel Sena Rios <marcelsenarios@gmail.com>
2019-09-19 17:59:22 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -38,6 +38,18 @@
             p.posicao = pos;
         }
 
+        public Peca retirarPeca(Posicao pos)
+        {
+            if (peca(pos) == null)
+            {
+                return null;
+            }
+            Peca aux = peca(pos);
+            aux.posicao = null;
+            pecas[pos.linha, pos.coluna] = null;
+            return aux;
+
+        }
 
         public bool posicaoValida(Posicao pos)
         {
